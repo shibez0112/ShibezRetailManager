05d61cf626a472ec64d12e4a042044287bfae5d8
shibez0112 <ginta2888@gmail.com>
2022-12-11 16:49:58 +0700

    Update EF Database Name
    Update the EF database name into EFData.

--- ShibezDataManager/Models/IdentityModels.cs
+++ ShibezDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ShibezDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
